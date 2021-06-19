b6663b00069e16f9746d3657f2dafb42193bc7e4
unknown <mlim@koolerindustries.com>
2021-06-19 14:28:58 +0800

    initial commit

---
+++ src/app/heroes-details/heroes-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-details',
+  templateUrl: './heroes-details.component.html',
+  styleUrls: ['./heroes-details.component.css']
+})
+export class HeroesDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
